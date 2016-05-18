3678b8b8eed47eb275f8c2a5a7a7a566d1453561
Jaofeng Chen <ChenJaofeng@JFs-MBP-R13.local>
2016-05-19 01:04:26 +0800

    完成 v1.00

--- Arduino/v2_I2C_1602/v2_I2C_1602.ino
+++ Arduino/v2_I2C_1602/v2_I2C_1602.ino
@@ -7,7 +7,7 @@
  ** 3. 亮度控制最大亮度 100% 最小亮度 5%
  ** 4. 可個別控制上下 UV 亮盤
  ** 5. 使用四個按鍵控制，按鍵各為：開始、模式、+/啟用、-/禁用
- ** 6. 自動模式下，時間終止會播放警示音並關閉 UV 亮盤
+ ** 6. 自動模式下，時間終止會播放警示音、閃爍LCD背光以及關閉 UV 亮盤
  ** 7. 手動模式下，使用「開始」鍵開啟與關閉 UV 上下亮盤
  ** 8. 自動記錄前次使用之設定值，避免 Reset 或斷電時需重新設定
  *********************************************/
@@ -60,19 +60,30 @@ const uint8_t MIN_TIMER = 10;
 const uint8_t TIMER_ADD_STEP = 10;
 // 警告音音頻
 const int SOUND_PULSE = 4435;
+// EEPROM 開始記錄位置
+const int MEM_BASE = 200; 
+// EEPROM 最大紀錄長度，單位 Bytes
+const int MAX_ALLOW_WRITES = 16;
 
 boolean pressStart = false;     // 是否按下開始鈕
 boolean pressMode = false;      // 是否按下模式鈕
 boolean pressCount = false;     // 是否按下數字增減鈕
 boolean starting = false;       // 是否開始曝光
 boolean bCountDown = false;     // 是否正在倒數中
-boolean playAlert = false;      // 是否播放警示音中
-uint16_t iCountDown = 120;      // 預設開始秒數
-uint16_t iStartDown = 0;        // 開始倒數的秒數
-byte iMode = 0;                 // 箭頭模式：0時間、1亮度、2上層、3下層、4手動模式
-byte iPanel = 3;                // UV燈板顯示模式；0-全不亮、1-上層、2-下層、3-上下層
-byte iLight = 80;               // 亮度百分比
-byte playIndex = 0;             // 正在播放的聲音順序序號
+boolean bPlayAlarm = false;     // 是否播放警示音中
+uint16_t iCountDown = 120;      // 倒數計數器
+uint16_t iStartDown = 0;        // 紀錄開始倒數的秒數
+uint8_t iMode = 0;              // 箭頭模式：0時間、1亮度、2上層、3下層、4手動模式
+uint8_t iPanel = 3;             // UV燈板顯示模式；0-全不亮、1-上層、2-下層、3-上下層
+uint8_t iLight = 80;            // 亮度百分比
+uint8_t playIndex = 0;          // 正在播放的聲音順序序號
+uint8_t hbval = 128;            // LED 呼吸燈亮度
+uint8_t hbdelta = 10;           // 呼吸燈每次增減的值
+
+// EEPROM 讀取值
+uint16_t romCountDown = 0;
+uint8_t romLight = 0;
+uint8_t romPanel = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);   // For PCF8574A
 
@@ -85,11 +96,33 @@ Thread atHeartbeat = Thread();
 Thread atCheckButton = Thread();
 // Count Down Thread
 Thread atCountDown = Thread();
+// Alarm Sound Thread
+Thread atPlayAlarm = Thread();
 
 // 程式進入點
 void setup() {
   Serial.begin(9600);
-  // 設定輸出 Pin 腳位
+  while (!Serial) {
+    ; // 等待 Serial Port 備妥
+  }
+  SetPinDefine();
+  EEPROM.setMemPool(MEM_BASE, EEPROMSizeATmega328);
+  EEPROM.setMaxAllowedWrites(MAX_ALLOW_WRITES);
+  ReadSettings();
+  SetThreads();
+  InitializationLCD();
+  tone(BUZZER_PIN, SOUND_PULSE);
+  delay(100);
+  noTone(BUZZER_PIN);
+}
+
+// 主迴圈
+void loop() {
+  controll.run();
+}
+
+// 設定 Pin 腳位定義與預設輸出
+void SetPinDefine() {
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(UP_PANEL_PIN, OUTPUT);
   pinMode(DN_PANEL_PIN, OUTPUT);
@@ -108,35 +141,30 @@ void setup() {
   digitalWrite(KEY_ADD_PIN, LOW);
   digitalWrite(KEY_SUB_PIN, LOW);
   digitalWrite(HEARTBEAT_PIN, HIGH);
-  delay(100);
-  ReadSettings();
-  InitializationLCD();
+}
 
+// 設定執行緒
+void SetThreads() {
   // Heartbeat Thread
   atHeartbeat.onRun(Heartbeat);
   atHeartbeat.setInterval(50);
+  controll.add(&atHeartbeat);
 
   // Check Button Thread
   atCheckButton.onRun(CheckButtons);
   atCheckButton.setInterval(220);
+  controll.add(&atCheckButton);
 
   // Count Down Thread
   atCountDown.onRun(CountDown);
   atCountDown.setInterval(500);
-
-  // Adds both threads to the controller
-  controll.add(&atHeartbeat);
-  controll.add(&atCheckButton); // & to pass the pointer to it
   controll.add(&atCountDown);
+  
+  // Alarm Sound Thread
+  atPlayAlarm.onRun(PlayAlarm);
+  atPlayAlarm.setInterval(100);
+  controll.add(&atPlayAlarm);
 }
-
-// 主迴圈
-void loop() {
-  controll.run();
-}
-
-uint8_t hbval = 128;  // LED亮度
-uint8_t hbdelta = 10;  // 每次增減的值
 
 // 顯示測試燈
 void Heartbeat() {
@@ -147,29 +175,28 @@ void Heartbeat() {
 
 // 倒數計時執行緒
 void CountDown() {
-  if (!starting)
+  if (!starting || iMode == 4)
     return;
   if (iCountDown > 0) {
     bCountDown = !bCountDown;
     if (!bCountDown) {
       iCountDown--;
-      ShowTime();
+      if (iMode != 4)
+        ShowTime();
     }
-  }
-  else
-  {
+  } else {
     digitalWrite(UP_PANEL_PIN, LOW);
     digitalWrite(DN_PANEL_PIN, LOW);
     starting = false;
     playIndex = 0;
-    playAlert = true;
+    bPlayAlarm = true;
     bCountDown = false;
   }
 }
 
 // 播放警示音
-void PlayAlert() {
-  if (!playAlert)
+void PlayAlarm() {
+  if (!bPlayAlarm)
     return;
   if (playIndex < 7 && playIndex % 2 == 0) {
     tone(BUZZER_PIN, SOUND_PULSE);
@@ -186,58 +213,54 @@ void PlayAlert() {
 void CheckButtons() {
   int isPress = HIGH;
   int v = 0;
-  /*
-  if (iMode <= 2) { // 開始鍵
-    isPress = digitalRead(KEY_START_PIN);
-    if (!pressStart && isPress == LOW) {  // 按下開始鍵並放開
-      Serial.print(millis());
-      Serial.print(F(" : Press Start, Mode="));
-      Serial.println(iMode);
-      if (iCountDown > 0) {   // 倒數器大於零，即正在倒數中
-        starting = !starting;
-        iStartDown = iCountDown;
-        if (starting) {   // 開始倒數
-          byte bLight = map(iLight, 0, 100, 0, 255);
-          switch (iMode) {  // 控制光盤顯示與否
-            case 0:
-              analogWrite(UP_PANEL_PIN, bLight);
-              digitalWrite(DN_PANEL_PIN, LOW);
-              break;
-            case 1:
-              digitalWrite(UP_PANEL_PIN, LOW);
-              analogWrite(DN_PANEL_PIN, bLight);
-              break;
-            case 2:
-              analogWrite(UP_PANEL_PIN, bLight);
-              analogWrite(DN_PANEL_PIN, bLight);
-              break;
-            default:
-              digitalWrite(UP_PANEL_PIN, LOW);
-              digitalWrite(DN_PANEL_PIN, LOW);
-              break;
-          }
-          ShowMenu(0);
-        } else {            // 停止或未開始倒數
-          digitalWrite(UP_PANEL_PIN, LOW);
-          digitalWrite(DN_PANEL_PIN, LOW);
-        }
-      }
-      if (playAlert) {
-        // 放警示音中
-        playAlert = false;
-        playIndex = 0;
-        noTone(BUZZER_PIN);
-        iCountDown = iStartDown;
-      }
-      pressStart = true;
+  isPress = digitalRead(KEY_START_PIN);
+  if (!pressStart && isPress == LOW) {  // 按下開始鍵並放開
+    if (bPlayAlarm) {
+      // 放警示音中
+      bPlayAlarm = false;
+      playIndex = 0;
+      noTone(BUZZER_PIN);
+      iCountDown = iStartDown;
+      lcd.backlight();
+      ShowTime();
       return;
     }
-    if (pressStart && isPress != LOW) {   // 放開開始鍵時
-      pressStart = false;
+    Serial.print(millis());
+    Serial.print(F(" : Press Start, Mode="));
+    Serial.println(iMode);
+    starting = !starting;
+    if (starting) {
+      // 閒置中；開始倒數，並依光線強度以 PWM 方式輸出
+      iStartDown = iCountDown;
+      byte bLight = map(iLight, 0, 100, 0, 255);
+      // 控制光盤顯示與否
+      if (bitRead(iPanel, 0) != 0)
+        analogWrite(UP_PANEL_PIN, bLight);
+      else
+        digitalWrite(UP_PANEL_PIN, LOW);
+      if (bitRead(iPanel, 1) != 0)
+        analogWrite(DN_PANEL_PIN, bLight);
+      else
+          digitalWrite(DN_PANEL_PIN, LOW);
+    } else {
+      // 倒數中，停止倒數並關閉 UV 光盤
+      digitalWrite(UP_PANEL_PIN, LOW);
+      digitalWrite(DN_PANEL_PIN, LOW);
     }
+    if (iMode == 4) {
+      // 手動開始/停止曝光
+    } else {
+      // 自動開始倒數
+      WriteSetting();
+      ShowTime();
+    }
+    pressStart = true;
+    return;
   }
-  */
-  if (!starting && !playAlert) {  
+  if (pressStart && isPress != LOW) {   // 放開開始鍵時
+    pressStart = false;
+  }
+  if (!starting && !bPlayAlarm) {  
     // 待機中
     isPress = digitalRead(KEY_MODE_PIN);
     if (!pressMode && isPress == LOW) {       // 按下模式鍵
@@ -265,7 +288,7 @@ void CheckButtons() {
       // 按下增加鍵或減少鍵
       Serial.print(millis());
       switch (iMode) {
-        case 0: // 亮盤秒數控制模式
+        case 0:   // 亮盤秒數控制模式
           if (pressSub == LOW) {
             Serial.print(F(" : Press [-] Key, Mode="));
             iCountDown -= TIMER_ADD_STEP;
@@ -283,7 +306,7 @@ void CheckButtons() {
             iCountDown = MIN_TIMER;
           ShowTime();
           break;
-        case 1: // 亮度控制模式
+        case 1:   // 亮度控制模式
           if (pressSub == LOW) {
             Serial.print(F(" : Press [-] Key, Mode="));
             iLight -= LIGHT_STEP;
@@ -298,7 +321,7 @@ void CheckButtons() {
             iLight = LIGHT_MIN;
           ShowLight();
           break;
-        case 2: // 上層
+        case 2:   // 上層
           if (pressSub == LOW) {
             Serial.print(F(" : Press [-] Key, Panel="));
             iPanel &= 0xFE;
@@ -309,7 +332,7 @@ void CheckButtons() {
           Serial.println(iPanel);
           ShowLedIcons(15);
           break;
-        case 3: // 下層
+        case 3:   // 下層
           if (pressSub == LOW) {
             Serial.print(F(" : Press [-] Key, Panel="));
             iPanel &= 0xFD;
@@ -320,13 +343,15 @@ void CheckButtons() {
           Serial.println(iPanel);
           ShowLedIcons(15);
           break;
-        case 4: // 手動模式
+        case 4:   // 手動模式
+          break;
+        default:  // 其他未定義
           break;
       }
-
       pressCount = true;
     }
-    if (pressCount && pressAdd != LOW && pressSub != LOW) { // 放開秒數/亮度鍵時 + 鍵
+    if (pressCount && pressAdd != LOW && pressSub != LOW) { 
+      // 放開秒數/亮度鍵時 + 鍵
       pressCount = false;
     }
   }
@@ -365,6 +390,7 @@ void InitializationLCD() {
   ShowMenu(0);
 }
 
+// 顯示主畫面
 void ShowMenu(byte menu) {
   String nv = "";
   switch (menu) {
@@ -382,13 +408,17 @@ void ShowMenu(byte menu) {
   }
 }
 
+// 顯示倒數時間
 void ShowTime() {
+  if (iMode == 4)
+    return;
   lcd.setCursor(1, 0); //Start at character 4 on line 0
   lcd.print(F("Time :"));
   lcd.print(RightPad(3, iCountDown));
   lcd.print(F("s  "));
 }
 
+// 顯示光盤亮度值
 void ShowLight() {
   lcd.setCursor(1, 1);
   lcd.print(F("Light:"));
@@ -396,6 +426,7 @@ void ShowLight() {
   lcd.print(F("%  "));
 }
 
+// 顯示選單游標
 void ShowCursor(byte mode) {
   lcd.setCursor(0, 0);
   lcd.print(F(" "));
@@ -432,6 +463,7 @@ void ShowCursor(byte mode) {
   }
 }
 
+// 將數字轉文字且將之靠右並左補空白
 String RightPad(uint8_t len, int val) {
   char nv[4];
   itoa (val, nv, 10);
@@ -443,6 +475,7 @@ String RightPad(uint8_t len, int val) {
   return nv1;
 }
 
+// 顯示光盤表示明滅的圖示
 void ShowLedIcons(uint8_t x) {
   lcd.setCursor(x, 0); //Start at character 4 on line 0
   if (bitRead(iPanel, 0) != 0)
@@ -456,14 +489,40 @@ void ShowLedIcons(uint8_t x) {
     lcd.write(byte(3));
 }
 
+// 自 EEPROM 中讀取記錄值
 void ReadSettings() {
-  
+  int sizeByte = sizeof(byte);
+  int sizeInt = sizeof(int);
+  romCountDown = EEPROM.readInt(MEM_BASE);
+  romLight = EEPROM.readByte(MEM_BASE + sizeInt);
+  romPanel = EEPROM.readByte(MEM_BASE + sizeInt + sizeByte);
+  if (romCountDown > 0)
+    iCountDown = romCountDown;
+  if (romLight > 0)
+    iLight = romLight;
+  if (romPanel > 0)
+    iPanel = romPanel;  
 }
 
+// 儲存記錄值至 EEPROM 中
 void WriteSetting() {
-  
+  int sizeByte = sizeof(byte);
+  int sizeInt = sizeof(int);
+  if (romCountDown != iStartDown) {
+    romCountDown = iStartDown;
+    EEPROM.writeInt(MEM_BASE, romCountDown);
+  }
+  if (romLight != iLight) {
+    romLight = iLight;
+    EEPROM.writeByte(MEM_BASE + sizeInt, romLight);
+  }
+  if (romPanel != iPanel) {
+    romPanel = iPanel;
+    EEPROM.writeByte(MEM_BASE + sizeInt + sizeByte, romPanel);
+  } 
 }
 
+// 設定自訂圖示
 void SetCustomChar() {
   byte upLightOn[8] = {
     B00000,
